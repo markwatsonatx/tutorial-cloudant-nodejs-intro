5229e544afdd6875a35b93db2c955a121948776f
Mark Watson <mark.watson.x@gmail.com>
2016-06-18 21:45:26 -0500

    Initial commit

---
+++ src/app/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    cloudant: {
+        hostname: "db",
+        username: "admin",
+        password: "pass"
+    },
+    express: {
+        port: 3000
+    }
+}
---
+++ src/app/server.js
@@ -0,0 +1,33 @@
+var cloudant = require('cloudant');
+var config = require("./config.js");
+var express = require('express');
+
+var app = express();
+
+var cloudantConn;
+
+// connect to cloudant
+cloudant(config.cloudant, function(er, cloudant, reply) {
+    cloudantConn = conn;
+    if (err) {
+        console.log('Could not connect to cloudant.');
+        console.log(err.message);
+    }
+    else {
+        console.log('Connection successful.');
+    }
+});
+
+// set view engine and map views directory
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// map requests
+app.get('/', function(req, res) {
+    res.render('index', {cloudantConnected: (cloudantConn != null)});
+});
+
+// start server
+app.listen(config.express.port, '0.0.0.0', function() {
+  console.log('Server started on port ' + config.express.port + '.')
+});
